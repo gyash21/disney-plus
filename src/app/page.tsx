a609c859e2816fe7cbfb2842b203f73fab69a3e5
Yash Gupta <gyash21@gmail.com>
2023-12-28 23:59:05 +0530

    update- remove styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>Let's build Disney Plus</h1>
+    <main className="bg">
+     <h1>Lets build Disney Plus</h1>
     </main>
   )
 }
